615ab69a02e2c1eb48832bf6054c06971c4aa04a
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-18 20:57:13 -0300

    ex 08 feito

---
+++ Bloco 04/4.1/ex08.js
@@ -0,0 +1,12 @@
+//🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+//Bonus: use somente um if.
+
+const a = 9
+const b = 21
+const c = 56
+
+if ( a % 2 == 0 || b % 2 == 0 || c % 2 ==0 ) {
+    console.log('True');
+} else {
+    console.log('False')
+}
