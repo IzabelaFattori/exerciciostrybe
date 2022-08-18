6b5a1c19cef4f5cb8bee7572fa391e8158b56e65
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-18 20:18:19 -0300

    exercicios 1-5 feitos

---
+++ Bloco 04/4.1/ex03.js
@@ -0,0 +1,17 @@
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const a = 8
+const b = 90
+const c = 50
+
+if (a > b && a > c) {
+    console.log(a)
+} else if (b > a && b > c) {
+    console.log(b)
+}else if (c > a && c > b) {
+    console.log(c)
+} else {
+    console.log('ERROR')
+} {
+    
+}
---
+++ Bloco 04/4.1/ex04.js
@@ -0,0 +1,11 @@
+//Faça um programa que, dado um valor recebido como parâmetro, retorne "positive" se esse valor for positivo, "negative" se for negativo, e caso não seja nem positivo e nem negativo retorne "zero".
+
+const a = 100
+
+if (a > 0){
+    console.log('Positivo')
+} else if (a < 0) {
+    console.log('Negativo')
+} else {
+    console.log('0')
+}
