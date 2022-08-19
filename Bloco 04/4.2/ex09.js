3a723c5f9840309b75e5496bbf7d1f3e8301cca9
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-19 18:34:09 -0300

    ex 09 feito

--- Bloco 04/4.2/ex09.js
+++ Bloco 04/4.2/ex09.js
@@ -1,1 +1,13 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+
+let lista = [];
+let result = [];
+
+
+for ( i = 0; i <= 25 ; i++){
+    lista.push(i)
+}
+for ( i = 0; i <= 25; i++){
+    result.push(lista[i] / 2)
+}
+console.log(result)
