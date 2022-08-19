5374ed1aafbb03da311cac4b0c6bc055e17b2a6f
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-19 18:26:23 -0300

    ex 07 feito

--- Bloco 04/4.2/ex07.js
+++ Bloco 04/4.2/ex07.js
@@ -1,5 +1,12 @@
 //Utilizando for, descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = Number.POSITIVE_INFINITY;
 
-for (i)
+for ( i = 0; i < numbers.length ; i += 1){
+    if (numbers[i] < menor){
+        menor = numbers[i]
+    }
+}
+
+console.log(menor)
