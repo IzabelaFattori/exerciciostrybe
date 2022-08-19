490b559e1fe5500d2bbf872488f0107c3b6df8bf
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-18 21:26:26 -0300

    ex 03 arrumado

--- Bloco 04/4.1/ex03.js
+++ Bloco 04/4.1/ex03.js
@@ -1,17 +1,16 @@
 //Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
-const a = 8
-const b = 90
-const c = 50
+const a = 0
+const b = 0
+const c = 8
 
-if (a > b && a > c) {
+
+if (( a == b == c) || (b == c & b < a) || (a > b && a > c)) {
     console.log(a)
-} else if (b > a && b > c) {
-    console.log(b)
-}else if (c > a && c > b) {
+} else if (( a == b & a < c) || (c > a && c > b)){
     console.log(c)
+} else if (( a == c & a < b) || (b > a && b > c)){
+    console.log(b)
 } else {
     console.log('ERROR')
-} {
-    
-}
+} 
