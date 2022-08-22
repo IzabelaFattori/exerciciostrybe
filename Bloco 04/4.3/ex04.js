608520ee2127f3efb5d070ca7045ef3398f0e9bb
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-22 19:23:12 -0300

    ex 04 feito

--- Bloco 04/4.3/ex04.js
+++ Bloco 04/4.3/ex04.js
@@ -1,1 +1,22 @@
 //4- Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 2 e 50.
+
+
+const primo = (numb) => {
+    for (let div = 2; div < numb; div++)
+      if (numb % div == 0) {
+        return false;
+      }
+  
+    return true;
+  };
+  
+  let numeroLimite = 50;
+  let maior = 0;
+  
+  for (let i = 2; i < numeroLimite; i++) {
+    if (primo(i)) {
+      maior = i;
+    }
+  }
+  
+  console.log(maior);
