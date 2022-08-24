a43f2240731526fdab36f160a29a140d12015e44
IzabelaFattori <izabela.fattori@hotmail.com>
2022-08-23 21:40:38 -0300

    ex 06 feito

--- Bloco 04/4.4/Parte I/ex06.js
+++ Bloco 04/4.4/Parte I/ex06.js
@@ -13,4 +13,6 @@ let leitor = {
     ],
   };
 
-    console.log('O livro favorito de ' + leitor.nome + leitor.sobrenome + 'se chama ' + leitor.livrosFavoritos.titulo)
+  let nomeDoLivro = leitor.livrosFavoritos[0];
+  
+  console.log('O livro favorito de ' + leitor.nome + ' ' +  leitor.sobrenome + ' se chama ' + nomeDoLivro['titulo'])
--- Bloco 04/4.4/Parte I/ex08.js
+++ Bloco 04/4.4/Parte I/ex08.js
@@ -1,1 +1,24 @@
 //🚀 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+let segundolivro = {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco', 
+}
+
+leitor.livrosFavoritos.push(segundolivro);
+
+console.log('Julia tem ' + leitor.livrosFavoritos + 'livros favoritos.')
