b222e251b7a47551d1129ec250cb0a08c866bac1
IzabelaFattori <izabela.fattori@hotmail.com>
2022-09-01 07:23:03 -0300

    Refazendo tudo

---
+++ Bloco 05/Dia 5.1/Ex aula/GetElementById./index01.js
@@ -0,0 +1,10 @@
+//01 --------------------------------------------------------
+
+document.getElementById("page-title").innerText = "Izabela";
+
+const change01 = document.getElementById("paragraph");
+change01.style.color = "red"
+
+document.getElementById("subtitle").innerText = "Subtitulo";
+
+document.getElementById('second-paragraph').innerText = "Outro pagrágrafo mesmo "
---
+++ Bloco 05/Dia 5.1/Ex aula/GetElementsByClassName and TagName/index02.js
@@ -0,0 +1,8 @@
+
+
+function imprimeItens(array) {
+    for (let index = 0; index < array.length; index = index + 1) {
+      console.log(array[index]);
+    }
+  }
+  
